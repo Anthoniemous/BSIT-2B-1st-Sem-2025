9096e189c089d6d68c7bfed6087975c7f10c6bfd
angeliligan <angelsiligan03@gmail.com>
2025-04-11 17:27:25 +0800

    Delete register.js

--- register.js
+++
@@ -1,9 +0,0 @@
-function signUp() {
-    document.getElementById('signup-form').style.display = 'none';
-
-    document.getElementById('login-form').style.display = 'block';
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('sign-up-btn').addEventListener('click', signUp);
-});
