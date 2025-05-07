c412e3d0a5acdb602fba24605ed78af153cb81d3
Akazaki21 <kawaii.ako12@gmail.com>
2025-05-07 16:41:00 +0800

    Added Apolinar.php

---
+++ Apolinar.php
@@ -0,0 +1,6 @@
+<?php
+    echo "This is echo line 1<br>","This is echo line 2<br>","This is echo line 3<br><br>";
+    print "This is print line 1<br>";
+    print "This is print line 2<br>";
+    print "This is print line 3<br>";
+?>
