f59ae85c4cdaa3305783f42cd73db0701cb62e97
marajoy-apolinar <maryjoyapolinar18@gmail.com>
2025-05-07 16:45:16 +0800

    Added Apolinar.php

--- Apolinar.php
+++ Apolinar.php
@@ -1,6 +1,8 @@
 <?php
-    echo "This is echo line 1<br>","This is echo line 2<br>","This is echo line 3<br><br>";
+    echo "This is echo line 1<br>","This is echo line 2<br>","This is echo line 2<br>","This is echo line 3<br><br>";
     print "This is print line 1<br>";
     print "This is print line 2<br>";
     print "This is print line 3<br>";
+    print "This is print line 4<br>";
+
 ?>
