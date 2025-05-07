b85b5da08650300421536a24b504c59f4471d090
Akazaki21 <kawaii.ako12@gmail.com>
2025-05-07 10:15:13 +0800

    Updated PanuncialmanPhpActivity.php

--- PanuncialmanPhpActivity.php
+++ PanuncialmanPhpActivity.php
@@ -9,15 +9,16 @@
             display: flex;
             justify-content: center;
             background-color: red;
+            font-size: 100px;
             color: yellow;
             padding: 50px;
-            width: 120px;
+            width: 400px;
         }
     </style>
 </head>
 <body>
     <?php
-        echo "This is an Echo<br>"," And also this is an echo <br>", "you can also add a code and style it like this<br>","<div class=\"box\"> Box1 </div>";
+        echo "This is an Echo<br>"," And also this is an echo <br>", "you can also add a code and style it like this<br>","<div class=\"box\">★</div>";
         print "This is a Print, but can't deploy many arguments";
     ?>
 </body>
