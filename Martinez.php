93718af3cf8224fd708683a79985ec1289423f8b
iTechuuuu_M <157936395+Brznv@users.noreply.github.com>
2025-05-07 13:01:28 +0800

    Martinez

---
+++ Martinez.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $value = print"Lol";
+        echo "eheeeeeee"." Ejay Martinez ". $value;
+        print $value;
+    ?>
+</body>
+</html>
