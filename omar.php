34461691bc8ca9ea86259a4b4f4133a6c423b35e
omarmacusang <mar.macusang.54@gmail.com>
2025-05-07 22:31:54 -0600

    okay ang life

---
+++ omar.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Echo and Print Example</title>
+</head>
+<body>
+
+<h1>PHP Output Example</h1>
+
+<?php
+   
+    echo "Hello, this is using echo!<br>";
+
+   
+    print "Hello, this is using print!";
+?>
+
+</body>
+</html>
