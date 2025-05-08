1f85d364065f28c4b6d86b28f3eb10afc8cf4d09
noymaemolde <moldenoymae@gmail.com>
2025-05-08 14:51:30 +0800

    gwapa ko sir

---
+++ molde.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+ echo "this is echo<br>","print 2nd line<br>";
+print "this is print<br>";
+print "print 2nd line<br>";
+
+
+
+  ?>
+  
+</body>
+</html>
